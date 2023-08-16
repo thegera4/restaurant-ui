587a77a72dcff9b47c030105c796d76a9b495b8d
thegera4 <84020433+thegera4@users.noreply.github.com>
2023-08-16 17:25:51 -0600

    working so far

--- src/app/orders/page.tsx
+++ src/app/orders/page.tsx
@@ -36,16 +36,15 @@ const OrdersPage = () => {
           </tr>
         </thead>
         <tbody>
-          {data?.map((order: OrderType) => {
-            <tr className="text-sm md:text-base bg-red-50" key={order.id}>
+          {data?.map((order: OrderType, index: number) => (
+            <tr className="text-sm md:text-base bg-red-50" key={index}>
               <td className="hidden md:block py-6 px-1">1237861238721</td>
               <td className="py-6 px-1">19.07.2023</td>
               <td className="py-6 px-1">89.90</td>
               <td className="hidden md:block py-6 px-1">Big Burger Menu (2), Veggie Pizza (2), Coca Cola 1L (2)</td>
               <td className="py-6 px-1">On the way (approx. 10min)...</td>
             </tr>
-          })
-          }
+          ))}
         </tbody>
       </table>
     </div>
