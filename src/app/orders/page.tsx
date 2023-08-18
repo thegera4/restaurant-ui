b850e5d565d29085c5f251fb10d81931dbee6933
thegera4 <84020433+thegera4@users.noreply.github.com>
2023-08-18 12:30:06 -0600

    fix: status updating function

--- src/app/orders/page.tsx
+++ src/app/orders/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 import { OrderType } from "@/types/types";
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
 import Image from "next/image";
@@ -21,14 +21,33 @@ const OrdersPage = () => {
       ),
   })
 
-  if (isLoading || status === "loading") return 'Loading...'
+  const queryClient = useQueryClient()
 
-  const handleUpdate = (e: React.FormEvent<HTMLFormElement>, orderId: string) => {
+  const mutation = useMutation({
+    mutationFn: ({id, status}: {id:string; status:string}) => {
+      return fetch(`http://localhost:3000/api/orders/${id}`, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({status}),
+      })
+    },
+    onSuccess() {
+      queryClient.invalidateQueries({queryKey: ['orders']})
+    }
+  })
+
+  const handleUpdate = (e: React.FormEvent<HTMLFormElement>, id: string) => {
     e.preventDefault()
     const form = e.target as HTMLFormElement;
     const input = form.elements[0] as HTMLInputElement;
     const status = input.value
+
+    mutation.mutate({id, status})
   }
+
+  if (isLoading || status === "loading") return 'Loading...'
   
   return (
     <div className="p-4 lg:px-20 xl:px-40">
